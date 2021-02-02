4d0cf19f22b5823e2e01e1484ced2ed13082a791
Michael Oshiro <oshiro@ucsb.edu>
2021-02-02 11:44:30 -0800

    display log working and comments

--- odmb_rad_test_sw.cxx
+++ odmb_rad_test_sw.cxx
@@ -8,9 +8,12 @@
 #endif 
 
 #include <cstdlib>
+#include <ctime>
+#include <iostream>
 #include <new>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include <windows.h>
 #include <winuser.h>
@@ -23,10 +26,13 @@ const int DIAG_ID_LOADCLOCKSYNTHFW = 4;
 const int DIAG_ID_EXIT = 5;
 const int DIAG_ID_WRITE_LOG = 6;
 
+const int DISPLAY_LOG_LENGTH = 16;
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 struct StateInfo {
   int counter;
+  std::vector<std::wstring> display_log;
 };
 
 //useful tutorial: http://www.winprog.org/tutorial/
@@ -107,11 +113,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow
 
   //text box
   HWND hwndCommentBox = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"test",
-      WS_CHILD | WS_VISIBLE, 16, 432, 160, 32, hwnd, (HMENU)DIAG_ID_COMMENTBOX, 
+      WS_CHILD | WS_VISIBLE, 16, 432, 544, 32, hwnd, (HMENU)DIAG_ID_COMMENTBOX, 
       NULL, NULL);
 
   //text
-  HWND hwndLogText = CreateWindowEx(0, L"STATIC", L"0", WS_CHILD | WS_VISIBLE, 
+  HWND hwndLogText = CreateWindowEx(0, L"STATIC", L"", WS_CHILD | WS_VISIBLE, 
       16, 16, 544, 336, hwnd, (HMENU)DIAG_ID_LOGTEXT, NULL, NULL);
 
   // Run the message loop.
@@ -134,6 +140,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
       CREATESTRUCT *pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
       StateInfo* AppState = reinterpret_cast<StateInfo*>(pCreate->lpCreateParams);
       AppState->counter = 0;
+      AppState->display_log = std::vector<std::wstring>(DISPLAY_LOG_LENGTH,L"");
       SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)AppState);
     }
     return 0;
@@ -163,8 +170,35 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         stros << AppState->counter;
         SetDlgItemText(hwnd, DIAG_ID_LOGTEXT, stros.str().c_str());
       }
-      if (LOWORD(wParam) == DIAG_ID_LOADFANOUTFW) {
-        system("C:\\cygwin64\\bin\\python3.6m.exe test.py");
+      else if (LOWORD(wParam) == DIAG_ID_LOADFANOUTFW) {
+        system("vivado -nojournal -nolog -mode batch -notrace -source load_fanout_fw.tcl");
+      }
+      else if (LOWORD(wParam) == DIAG_ID_WRITE_LOG) {
+        LONG_PTR AppStateLongPtr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+        StateInfo* AppState = reinterpret_cast<StateInfo*>(AppStateLongPtr);
+
+        //update log
+        for (unsigned log_idx = 0; log_idx < (DISPLAY_LOG_LENGTH-1); log_idx++) {
+          AppState->display_log[log_idx] = AppState->display_log[log_idx+1];
+        }
+        wchar_t diag_comment_text[256];
+        GetDlgItemText(hwnd, DIAG_ID_COMMENTBOX, diag_comment_text, 256);
+        std::time_t current_time = std::time(nullptr);
+        char* time_cstr = std::asctime(std::localtime(&current_time));
+        std::string time_string = (std::string(time_cstr)).substr(4,20);
+        //convert from std::string to std::wstring assuming all characters are single-byte
+        AppState->display_log[DISPLAY_LOG_LENGTH-1] = 
+            std::wstring(time_string.begin(), time_string.end()) + L": " + std::wstring(diag_comment_text);
+
+        //write to box
+        std::wostringstream stros;
+        for (unsigned log_idx = 0; log_idx < DISPLAY_LOG_LENGTH; log_idx++) {
+          stros << AppState->display_log[log_idx] << "\n";
+        }
+        SetDlgItemText(hwnd, DIAG_ID_LOGTEXT, stros.str().c_str());
+      }
+      else if (LOWORD(wParam) == DIAG_ID_EXIT) {
+        PostQuitMessage(0);
       }
     }
     return 0;
